bd07ae98718992ec11c825c1fcfced8ff282f022
Akash Ahmed <akashahmed5724@gmail.com>
2022-08-25 16:41:20 +0600

    videos+videos2+transactions db add

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const todos = require("./data/todos");
+const videos = require("./data/videos");
+const videos2 = require("./data/videos2");
+const transactions = require("./data/transactions");
 
 module.exports = {
   todos,
+  videos,
+  videos2,
+  transactions,
 }
 
