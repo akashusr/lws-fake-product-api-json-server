2bdb286f94691717e6f92ac3afbac3114872b129
Akash Ahmed <akashahmed5724@gmail.com>
2022-08-25 14:39:32 +0600

    post

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 server.use(middlewares);
 server.use(router);
