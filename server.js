f5b721176089f7a741fa17f68994cd642d42b0f6
Sumit Saha <sumit@learnwithsumit.com>
2022-09-02 19:11:21 +0600

    changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const data = require("./db.js");
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(data);
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
+
+// Bind the router db to the app
+server.db = router.db;
 
 server.use(middlewares);
 server.use(router);
